e6deb7cd7fa1ae2aff3f54188a2d137fd302abfc
Ankit Kumar <kumarankit0520@gmail.com>
2025-04-25 12:59:24 +0530

    configured for build , solving routing and ssr error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'destinations/:district',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'destination/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
---
+++ src/app/prerender.config.ts
@@ -0,0 +1,14 @@
+// src/prerender.config.ts
+
+export function getPrerenderParams() {
+    return [
+      { route: '/destinations/gangtok' },
+      { route: '/destinations/east' },
+      { route: '/destinations/west' },
+      { route: '/destinations/north' },
+      { route: '/destinations/south' },
+      { route: '/destination/1' },
+      { route: '/destination/2' }
+    ];
+  }
+  
