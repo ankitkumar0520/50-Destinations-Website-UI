0b54f1a93a4e086a7e209b8ea6fd28562f97b14d
Ankit Kumar <kumarankit0520@gmail.com>
2025-04-30 13:09:41 +0530

    adjusted snow fall, heritage sikkim icon layout in hero and added loader and error loading overlay in pdf viewer

--- src/app/pages/events/view-pdf/view-pdf.component.ts
+++ src/app/pages/events/view-pdf/view-pdf.component.ts
@@ -2,14 +2,17 @@ import { Component, inject, Input, OnInit, ViewChild } from '@angular/core';
 import {  pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
 import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 import { EventService } from '../../../services/event.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-view-pdf',
   templateUrl: './view-pdf.component.html',
   styleUrls: ['./view-pdf.component.css'],
-  imports: [NgxExtendedPdfViewerModule]
+  imports: [NgxExtendedPdfViewerModule,CommonModule]
 })
 export class ViewPdfComponent implements OnInit {
   @Input() pdfUrl: string = '';
+  loading:boolean = false;
+  loadingFailed:boolean=false;
 
   eventService: EventService = inject(EventService);
 
@@ -24,6 +27,7 @@ export class ViewPdfComponent implements OnInit {
     this.pdfName = this.pdfUrl.split('/').pop() || 'default.pdf';
   }
   
+
 
 
   
