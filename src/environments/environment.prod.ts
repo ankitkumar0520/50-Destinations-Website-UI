1e5e98f1d0a2e3a34ad5cdde98fbc89ce727c3f9
dloc47 <deepakchettri877@gmail.com>
2025-05-13 00:16:48 +0530

    Added server config in environment.ts and environment.prod.ts and commented it.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
- // apiBaseUrl: 'http://147.93.98.211:5000/api',
+ // apiBaseUrl: 'https://147.93.98.211:5000/api',
+ // apiBaseUrl: 'https://api.sikkimdarshan.com/api',
   apiBaseUrl: 'https://localhost:44321/api', //for development
 };
