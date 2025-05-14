e93d15ad9c6e1983309c9a696ec4f6e61e3565d4
Pranai Giri <102620441+pranaigiri@users.noreply.github.com>
2025-05-15 01:10:22 +0530

    UI Update, margin fixed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,6 @@
 export const environment = {
-  production:   true,
- // apiBaseUrl: 'https://147.93.98.211:5000/api',
- apiBaseUrl: 'https://api.sikkimdarshan.com/api',
-  //apiBaseUrl: 'https://localhost:44321/api', //for development
-
+  production: true,
+  // apiBaseUrl: 'https://147.93.98.211:5000/api',
+  apiBaseUrl: 'https://api.sikkimdarshan.com/api',
+  // apiBaseUrl: 'https://localhost:44321/api', //for development
 };
